611c1957505d68c2d145357c33236b0e9c8f0c02
Shyam Joshi <kanojoshiiman@gmail.com>
2025-06-11 17:47:09 +0530

    register user component setup

--- client/src/pages/auth/register.jsx
+++ client/src/pages/auth/register.jsx
@@ -1,10 +1,9 @@
 import React, { useState } from "react";
-import { useToast } from "@/components/ui/";
 import { Link, useNavigate } from "react-router-dom";
 import { registerFormControls } from "@/config";
 import CommonForm from "@/components/common/forms";
 import { useDispatch } from "react-redux";
-import { registerUser } from "@/store/auth-slice";
+import { registerUser } from "@/store/authSlice";
 
 const initialState = {
   userName: "",
@@ -15,23 +14,22 @@ const AuthRegister = () => {
   const [formData, setFormData] = useState(initialState);
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const { toast } = useToast();
 
   function onSubmit(e) {
-    e.preventDefault();
-    dispatch(registerUser(formData)).then((data) => {
-      if (data?.payload?.success) {
-        toast({
-          title: data?.payload?.message,
-        });
-        navigate("/auth/login");
-      } else {
-        toast({
-          title: data?.payload?.message,
-          variant: "destructive",
-        });
-      }
-    });
+    // e.preventDefault();
+    // dispatch(registerUser(formData)).then((data) => {
+    //   if (data?.payload?.success) {
+    //     toast({
+    //       title: data?.payload?.message,
+    //     });
+    //     navigate("/auth/login");
+    //   } else {
+    //     toast({
+    //       title: data?.payload?.message,
+    //       variant: "destructive",
+    //     });
+    //   }
+    // });
   }
   console.log(formData);
   return (
