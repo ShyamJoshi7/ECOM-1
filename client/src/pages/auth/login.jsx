67695f377954e282e518873375e487ebe3a49442
Shyam Joshi <kanojoshiiman@gmail.com>
2025-06-11 09:40:34 +0530

    auth pages completed

---
+++ client/src/pages/auth/login.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { loginFormControls } from "@/config";
+import CommonForm from "@/components/common/forms";
+
+const initialState = {
+  email: "",
+  password: "",
+};
+
+function AuthLogin() {
+  const [formData, setFormData] = useState(initialState);
+
+  function onSubmit(e) {
+    e.preventDefault();
+    dispatch(loginUser(formData)).then((data) => {
+      if (data?.payload?.success) {
+        toast({
+          title: data?.payload?.message,
+        });
+      } else {
+        toast({
+          title: data?.payload?.message,
+          variant: "destructive",
+        });
+      }
+    });
+  }
+
+  return (
+    <div className="mx-auto w-full max-w-md space-y-6">
+      <div className="text-center">
+        <h1 className="text-3xl font-bold tracking-tight text-foreground">
+          Sign in to your account
+        </h1>
+        <p className="mt-2">
+          Don't have an account
+          <Link
+            className="font-medium ml-3 text-primary hover:underline"
+            to="/auth/register"
+          >
+            Register
+          </Link>
+        </p>
+      </div>
+      <CommonForm
+        formControls={loginFormControls}
+        buttonText={"Sign In"}
+        formData={formData}
+        setFormData={setFormData}
+        onSubmit={onSubmit}
+      />
+    </div>
+  );
+}
+
+export default AuthLogin;
---
+++ client/src/pages/auth/register.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import { useToast } from "@/components/ui/";
+import { Link, useNavigate } from "react-router-dom";
+import { registerFormControls } from "@/config";
+import CommonForm from "@/components/common/forms";
+import { useDispatch } from "react-redux";
+import { registerUser } from "@/store/auth-slice";
+
+const initialState = {
+  userName: "",
+  email: "",
+  password: "",
+};
+const AuthRegister = () => {
+  const [formData, setFormData] = useState(initialState);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { toast } = useToast();
+
+  function onSubmit(e) {
+    e.preventDefault();
+    dispatch(registerUser(formData)).then((data) => {
+      if (data?.payload?.success) {
+        toast({
+          title: data?.payload?.message,
+        });
+        navigate("/auth/login");
+      } else {
+        toast({
+          title: data?.payload?.message,
+          variant: "destructive",
+        });
+      }
+    });
+  }
+  console.log(formData);
+  return (
+    <div className="mx-auto w-full max-w-md space-y-6">
+      <div className="text-center">
+        <h1 className="text-3xl tracking-tight font-bold text-foreground">
+          Create New Account
+        </h1>
+        <p className="mt-2">
+          Already have an account
+          <Link
+            className="font-medium ml-2 text-primary hover:underline"
+            to="/auth/login"
+          >
+            Login
+          </Link>
+        </p>
+      </div>
+      <CommonForm
+        formControls={registerFormControls}
+        buttonText={"Sign Up"}
+        formData={formData}
+        setFormData={setFormData}
+        onSubmit={onSubmit}
+      />
+    </div>
+  );
+};
+
+export default AuthRegister;
