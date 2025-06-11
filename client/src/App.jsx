7601eedaade1ea68f3f2c9ea99ca7b6d209939ad
Shyam Joshi <kanojoshiiman@gmail.com>
2025-06-11 17:25:37 +0530

    Route setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,71 @@
 import React from "react";
-import { Button } from "@/components/ui/button";
+import { Route, Routes } from "react-router-dom";
+import AuthLayout from "./components/auth/layout";
+import Authlogin from "./pages/auth/login";
+import AuthRegister from "./pages/auth/register";
+import AdminLayout from "./components/admin-view/layout";
+import AdminDashboard from "./pages/admin-view/dashboard";
+import AdminFeatures from "./pages/admin-view/features";
+import AdminOrders from "./pages/admin-view/orders";
+import AdminProducts from "./pages/admin-view/products";
+import UnauthPage from "./pages/unauthPage";
+import ShoppingLayout from "./components/shopping-view/layout";
+import Notfound from "./pages/notfound";
+import Home from "./pages/shopping-view/Home";
+import Listing from "./pages/shopping-view/listing";
+import Account from "./pages/shopping-view/account";
+import Checkout from "./pages/shopping-view/checkout";
+import CheckAuth from "./components/common/check-auth";
+
 const App = () => {
-  return <div>ao</div>;
+  const isAuthenticated = false;
+  const user = null;
+
+  return (
+    <div className="flex flex-col overflow-hidden bg-white">
+      <Routes>
+        <Route
+          path="/auth"
+          element={
+            <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+              <AuthLayout />
+            </CheckAuth>
+          }
+        >
+          <Route path="login" element={<Authlogin />} />
+          <Route path="register" element={<AuthRegister />} />
+        </Route>
+        <Route
+          path="/admin"
+          element={
+            <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+              <AdminLayout />
+            </CheckAuth>
+          }
+        >
+          <Route path="dashboard" element={<AdminDashboard />} />
+          <Route path="products" element={<AdminProducts />} />
+          <Route path="orders" element={<AdminOrders />} />
+          <Route path="features" element={<AdminFeatures />} />
+        </Route>
+        <Route
+          path="/collection"
+          element={
+            <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+              <ShoppingLayout />
+            </CheckAuth>
+          }
+        >
+          <Route path="home" index element={<Home />} />
+          <Route path="listing" element={<Listing />} />
+          <Route path="checkout" element={<Checkout />} />
+          <Route path="account" element={<Account />} />
+        </Route>
+        <Route path="*" element={<Notfound />} />
+        <Route path="unauth-page" element={<unauthPage />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
