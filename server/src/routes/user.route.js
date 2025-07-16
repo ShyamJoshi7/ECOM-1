0ad0b358a6724474c381fd8e322d0892ebe90e3b
Shyam Joshi <kanojoshiiman@gmail.com>
2025-07-16 13:22:13 +0530

    User Routing part completed

--- server/src/routes/user.route.js
+++ server/src/routes/user.route.js
@@ -5,13 +5,14 @@ const { authenticateToken } = require("../middlewares/auth.middleware");
 const router = express.Router();
 
 router.post("/register", controller.registerUser);
-// router.post("/login", controller.loginUser);
-router.post("/send/otp", controller.sendResetPasswordOtp);
-router.post("/reset/password", controller.resetPasswordWithOtp);
-router.put("/update/profile", [authenticateToken], controller.updateProfile);
-router.patch(
-  "/update/password",
-  [authenticateToken],
-  controller.updatePassword
-);
+router.post("/login", controller.loginUser);
+router.post("/logout", controller.logoutUser);
+// router.post("/send/otp", controller.sendResetPasswordOtp);
+// router.post("/reset/password", controller.resetPasswordWithOtp);
+// router.put("/update/profile", [authenticateToken], controller.updateProfile);
+// router.patch(
+//   "/update/password",
+//   [authenticateToken],
+//   controller.updatePassword
+// );
 module.exports = router;
