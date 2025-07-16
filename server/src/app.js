b377dffeaedffdf1a32e4e81bdefbd1616a55f25
Shyam Joshi <kanojoshiiman@gmail.com>
2025-07-16 13:18:34 +0530

    server side setup for auth router

--- server/src/app.js
+++ server/src/app.js
@@ -15,10 +15,10 @@ const {
 const userRoutes = require("./routes/user.route");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware stack
-app.use(helmet());
+
 app.use(
   cors({
     origin: process.env.CLIENT_URL || "http://localhost:5173",
@@ -33,11 +33,11 @@ app.use(
     credentials: true,
   })
 );
-app.use(cookieParser());
-app.use(express.json());
+app.use(helmet());
 app.use(compression());
-app.use(morgan("dev"));
-app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
@@ -45,6 +45,11 @@ const limiter = rateLimit({
   message: "Too many requests, try again later.",
 });
 app.use(limiter);
+
+// 3. Logger (only in dev)
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 // Connct to MongoDB
 attemptConnnection();
@@ -58,7 +63,12 @@ app.use("/auth", userRoutes);
 // app.use("/payment", paymentRoutes);
 
 // Middleware
-app.post("/dangerous/stop/backend", stopBackendMiddleware);
+if (
+  process.env.ENABLE_KILL_SWITCH === "true" &&
+  process.env.NODE_ENV !== "production"
+) {
+  app.post("/dangerous/stop/backend", stopBackendMiddleware);
+}
 app.use(notFoundMiddleware);
 app.use(internalServerErrorMiddleware);
 
