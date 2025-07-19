401a200c0ea4a3b3e7318ba9ae207bac5d23efe9
Shyam Joshi <kanojoshiiman@gmail.com>
2025-07-19 08:04:41 +0530

    Admin Header started

--- client/src/components/admin-view/header.jsx
+++ client/src/components/admin-view/header.jsx
@@ -1,7 +1,33 @@
+import { AlignJustify, LogOut } from "lucide-react";
 import React from "react";
+import { Button } from "../ui/button";
+import { useDispatch } from "react-redux";
+import { logoutUser } from "@/store/authSlice";
 
 const AdminHeader = () => {
-  return <div>AdminHeader</div>;
+  const dispatch = useDispatch();
+
+  const handleLogout = () => {
+    dispatch(logoutUser());
+  };
+
+  return (
+    <header className="flex items-center justify-between px-4 py-3 bg-background border-b ">
+      <Button className="lg:hidden sm:block">
+        <AlignJustify />
+        <span className="sr-only">Toggele Menu</span>
+      </Button>
+      <div className="flex flex-1 justify-end">
+        <Button
+          onClick={handleLogout}
+          className="inline-flex gap-2 items-center rounded-md px-4 py-2 text-sm font-medium shadow"
+        >
+          <LogOut />
+          Logout
+        </Button>
+      </div>
+    </header>
+  );
 };
 
 export default AdminHeader;
