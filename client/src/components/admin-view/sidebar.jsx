6659fa6e2726c965d6da225e0da617c090c872b1
Shyam Joshi <kanojoshiiman@gmail.com>
2025-07-23 01:45:09 +0530

    SideBar started

--- client/src/components/admin-view/sidebar.jsx
+++ client/src/components/admin-view/sidebar.jsx
@@ -58,21 +58,33 @@ const MenuItems = ({ setOpen }) => {
   );
 };
 
-const AdminSidebar = () => {
+const AdminSidebar = ({ open, setOpen }) => {
   const navigate = useNavigate();
 
   return (
     <Fragment>
+      <Sheet open={open} onOpenChange={setOpen}>
+        <SheetContent side="left" className="w-64">
+          <div className="flex flex-col h-full">
+            <SheetHeader className="border-b">
+              <SheetTitle className="flex gap-2 mt-5 mb-5">
+                <ChartNoAxesCombined size={30} />
+                <h1 className="text-2xl font-extrabold">Admin Panel</h1>
+              </SheetTitle>
+            </SheetHeader>
+            <MenuItems setOpen={setOpen} />
+          </div>
+        </SheetContent>
+      </Sheet>
       <aside className="hidden w-64 flex-col border-r bg-background p-6 lg:flex">
         <div
-          onClick={() => {
-            navigate("/admin/dashboard");
-          }}
+          onClick={() => navigate("/admin/dashboard")}
           className="flex cursor-pointer items-center gap-2"
         >
           <ChartNoAxesCombined size={30} />
-          <h1 className="text-xl font-extrabold">Admin Panel</h1>
+          <h1 className="text-2xl font-extrabold">Admin Panel</h1>
         </div>
+        <MenuItems />
       </aside>
     </Fragment>
   );
