651ff2fbfd0f9c4bba116c0813bb3f2c570668fb
Shyam Joshi <kanojoshiiman@gmail.com>
2025-07-22 07:45:20 +0530

    Admin SideBar Start

--- client/src/components/admin-view/sidebar.jsx
+++ client/src/components/admin-view/sidebar.jsx
@@ -1,12 +1,59 @@
-import { adminSidebarMenuItems } from "@/config";
-import { ChartNoAxesCombined } from "lucide-react";
+import {
+  BadgeCheck,
+  ChartNoAxesCombined,
+  LayoutDashboard,
+  ShoppingBasket,
+} from "lucide-react";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+
 import React, { Fragment } from "react";
 import { useNavigate } from "react-router-dom";
 
-const MenuItems = () => {
+const adminSidebarMenuItems = [
+  {
+    id: "dashboard",
+    label: "Dashboard",
+    path: "/admin/dashboard",
+    icon: <LayoutDashboard />,
+  },
+  {
+    id: "products",
+    label: "Products",
+    path: "/admin/products",
+    icon: <ShoppingBasket />,
+  },
+  {
+    id: "orders",
+    label: "Orders",
+    path: "/admin/orders",
+    icon: <BadgeCheck />,
+  },
+];
+
+const MenuItems = ({ setOpen }) => {
+  const navigate = useNavigate();
   return (
     <nav className="mt-8 flex-col flex gap-2">
-      {adminSidebarMenuItems.map()}
+      {adminSidebarMenuItems.map((menuItem) => (
+        <div
+          key={menuItem.id}
+          onClick={() => {
+            navigate(menuItem.path);
+            setOpen ? setOpen(false) : null;
+          }}
+          className="flex items-center gap-2 rounded-md px-3 py-2 text-muted-foreground hover:bg-muted hover:text-foreground"
+        >
+          {menuItem.icon}
+          <span>{menuItem.label}</span>
+        </div>
+      ))}
     </nav>
   );
 };
